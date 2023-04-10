dd2fd74492f7e141a9e5e6800f98e019c1b9f2cb
Sddilora <sumeyyedilaradogan@gmail.com>
2023-04-10 05:11:53 +0300

    axios.get has some problems, still unsolved

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-const PORT = 8000
-const cheerio = require('cheerio')
-const express = require('express')
-const axios = require('axios')
+const PORT = 8000;
+const cheerio = require("cheerio");
+const express = require("express");
+const axios = require("axios").default;
 
-const app = express()
-challenges = [] // They are an array we indicate that in here
+const app = express();
+// challenges = [] // They are an array we indicate that in here
 
-app.get('/hello', (req, res) => {
-    res.json('Hello, Welcome to my Stream')
-})
+app.get("/hello", (req, res) => {
+  res.json("Hello, Welcome to my Stream");
+});
 
-app.get('/challenges', (req, res) => {
-    axios.get('https://ghw.mlh.io/challenges')
-        .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
+app.get("/challenges", (req, res) => {
+  try {
+    var challenges = [];
+    var html = "";
+    axios.get("https://ghw.mlh.io/challenges")
+    .then((response) => {
+      html = response.data;
+    })
+    .catch (function(error) {
+    console.error(error);
+    })
+    
+    res.status(200).json(html);
 
-            $('a:contains("")', html).each(function () {
-                const title = $(this).text()
-                const url = $(this).attr('href')
-                challenges.push({
-                    title,
-                    url
-                })
-            })
-        })
-    res.json(challenges)
-})
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
 
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
